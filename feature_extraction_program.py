edc9ba358c1dd97b60dbb563c3ecd4c9b180d175
Johan Öfverstedt <johanofverstedt@users.noreply.github.com>
2025-10-07 14:27:29 +0200

    Fixed some bugs in the parameters.

--- feature_extraction_program.py
+++ feature_extraction_program.py
@@ -33,7 +33,7 @@ def supervoxel_feature_extraction_from_files(supervoxel_path, ff_folder, jacdet_
     bbox_cache = None
     start_index = 0
     
-    output_path = f'{out_folder}/{feature_set_name}.npz'
+    output_path = f'{out_folder}/features_{feature_set_name}.npz'
     os.makedirs(out_folder, exist_ok=True)
     
     if os.path.exists(output_path):
@@ -45,33 +45,42 @@ def supervoxel_feature_extraction_from_files(supervoxel_path, ff_folder, jacdet_
         t1 = time.time()
         subj = subject_ids[i]
         
-        ff_path = ff_folder + f'ff_{subj}{ext}'
-        jacdet_path = jacdet_folder + f'jd_{subj}{ext}'
+        ff_path = f'{ff_folder}/ff_{subj}{ext}'
+        jacdet_path = f'{jacdet_folder}/jd_{subj}{ext}'
+        print(f'Reading ff image {ff_path}')
         
-        ff_image = sitk.ReadImage(ff_image, sitk.sitkFloat32)
+        ff_image = sitk.ReadImage(ff_path, sitk.sitkFloat32)
+
+        print(f'Reading jd image {jacdet_path}')
         jacdet_image = sitk.ReadImage(jacdet_path, sitk.sitkFloat32)
+        
         ff_array = sitk.GetArrayFromImage(ff_image)
-        jacdet_array = sitk.GetArrayFromImage(jacet_image)
+        jacdet_array = sitk.GetArrayFromImage(jacdet_image)
         
         # compute the voxel size in mL
-        voxel_size = np.prod(np.array(ff_image.GetSpacing())) / 1000.0
+        sp = np.array(ff_image.GetSpacing())
+        voxel_size = np.prod(sp) / 1000.0
+        if i == 0:
+            print(f'Voxel size (mL): {voxel_size}, spacing in mm: {sp}')
                 
-        if single_tissue:
-            features_i, bbox_cache = fe.extract_single_tissue_features_from_numpy_arrays(supervoxel_array, supervoxel_indices, deformed_ff_image, jacdet_image, single_voxel_volume=voxel_size, bbox_cache=bbox_cache, return_bbox_cache=True)
+        if single_tissue == True:
+            print('Extracting single tissue features')
+            features_i, bbox_cache = fe.extract_single_tissue_features_from_numpy_arrays(supervoxel_array, supervoxel_indices, ff_array, jacdet_array, single_voxel_volume=voxel_size, bbox_cache=bbox_cache, return_bbox_cache=True)
         else:
-            features_i, bbox_cache = fe.extract_tissue_specific_features_from_numpy_arrays(supervoxel_array, supervoxel_indices, deformed_ff_image, jacdet_image, single_voxel_volume=voxel_size, bbox_cache=bbox_cache, return_bbox_cache=True)
+            print('Extracting tissue-specific features')
+            features_i, bbox_cache = fe.extract_tissue_specific_features_from_numpy_arrays(supervoxel_array, supervoxel_indices, ff_array, jacdet_array, single_voxel_volume=voxel_size, bbox_cache=bbox_cache, return_bbox_cache=True)
         
         all_features.append(features_i)
 
         t2 = time.time()
         print(f'Processed subject {i+1} with id {subj}. Time elapsed: {t2-t1}s.')
-        if (i - start_index ) % save_period) == save_period-1:
+        if ((i - start_index ) % save_period) == save_period-1:
             print(f'Saving features ({i} processed)')
             cat_features = np.concatenate(all_features, axis=0)
             np.savez_compressed(output_path, cat_features, allow_pickle=True)
             
     cat_features = np.concatenate(all_features, axis=0)   
-    print(f'--- DONE --- Saving features ({cat_features.shape[0]} processed)')
+    print(f'--- DONE --- Saving features ({cat_features.shape[0]} processed). Shape of feature array: {cat_features.shape}')
     np.savez_compressed(output_path, cat_features, allow_pickle=True)
     
 def main():
@@ -80,8 +89,9 @@ def main():
     parser.add_argument('ff_folder', help='The folder containing the ff (fat fraction) image files with the file pattern ff_subj.nii.gz')
     parser.add_argument('jacdet_folder', help='The folder containing the jacdet (Jacobian Determinants) image files with the file pattern jd_subj.nii.gz')
     parser.add_argument('subject_id_path', help='The full path to a text file containing one subject id on each line.')
-    parser.add_argument('single_tissue', action='store_true', help='Do feature extraction for a single tissue group')
-    parser.add_argument('ext', default='.nii.gz', help='File extension of the image files')
+    parser.add_argument('name', help='The name of this set of features')
+    parser.add_argument('-s', '--single_tissue', default='False', action='store_true', help='Do feature extraction for a single tissue group')
+    parser.add_argument('-e', '--ext', default='.nii.gz', help='File extension of the image files')
  
     args = parser.parse_args()
     
@@ -94,11 +104,12 @@ def main():
         return
     
     with open(args.subject_id_path) as file:
-        subject_ids = [line.rstrip() for line in file]
+        subject_ids = [line.rstrip() for line in file if len(line.rstrip()) > 0]
 
     print(f'Number of subjects to extract features for: {len(subject_ids)}')
+    print(f'Single tissue?: {args.single_tissue}')
     
-    supervoxel_feature_extraction_from_files(args.supervoxel_path, args.ff_folder, args.jacdet_folder, subject_ids, single_tissue = args.single_tissue, ext=args.ext)
+    supervoxel_feature_extraction_from_files(args.supervoxel_path, args.ff_folder, args.jacdet_folder, subject_ids, './outputs', feature_set_name = args.name, single_tissue = args.single_tissue==True, ext=args.ext)
    
 if __name__ == '__main__':
     main()
